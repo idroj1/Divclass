6b2d3ea5ca7b624af1597b66f88c5b6466f784c7
idroj1 <52473617+idroj1@users.noreply.github.com>
2021-09-14 02:47:15 -0400

    Update GroupList.cs

--- Clist/GroupList.cs
+++ Clist/GroupList.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collection.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Clist
 {
     public class GroupList
     {
-        public static Dictionary<string, List<string>> Groups, List<string> Topic()
+        public static Dictionary<string, List<string>> GroupsTopics(List<string> Person, List<string> Topic)
         {
+            if(Person==null||!Person.Any()||Topic==null||!Topic.Any())
+            {
+                throw new IndexOutOfRangeException();
+            }
+            var Group = new Dictionary<List<string>, List<string>>();
 
+
+
+            //return new Dictionary<string, List<string>>();
         }
     }
 }
